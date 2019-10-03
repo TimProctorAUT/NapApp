db1da95bde20844560b5ccbdafada597aa460660
ghf4336 <42361147+ghf4336@users.noreply.github.com>
2019-10-03 21:07:10 +1300

    Update testDataPage.dart

--- nap_app/lib/views/testDataPage.dart
+++ nap_app/lib/views/testDataPage.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:first_app/setting.dart';
+import 'package:first_app/setting.dart';
+
+import 'package:wakelock/wakelock.dart';
+import 'testDataPage.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'aboutPage.dart' as AboutPage;
+
+
 
 class TestData extends StatefulWidget
 {
+  
+ final NapSettingsData settings;
+
+  TestData({this.settings});
+  
+  
+  
   @override
   _TestDataPageState createState() => _TestDataPageState();
 }
 
 
+
 class _TestDataPageState extends State<TestData> {
+  
   @override
   Widget build(BuildContext context) {
+    int  hello = widget.settings.napLength;
+    String oneAsString = hello.toString();
     return MaterialApp(
       title: 'NapApp Menu Tests',
       theme: ThemeData(
@@ -18,13 +40,17 @@ class _TestDataPageState extends State<TestData> {
         primaryColor: Color.fromRGBO(18, 18, 18, 1),
         scaffoldBackgroundColor: Color.fromRGBO(18, 18, 18, 1),
         cardColor: Color.fromRGBO(18, 18, 18, 0.8),
+        
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Summary Page'),
+          
+          title: Text('Summary Page'+ oneAsString),
+          
         ),
         body: Row(    
       mainAxisAlignment: MainAxisAlignment.center,
+      
       children: <Widget>[
         Column( 
 
@@ -32,7 +58,7 @@ class _TestDataPageState extends State<TestData> {
             
 
             Text(
-              'Who Are We?',
+              'Your Nap Time',
               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
 
             ),
@@ -50,14 +76,15 @@ class _TestDataPageState extends State<TestData> {
             ),
 
             Text(
-              'Why Should you Nap?',
+              'Your Start Time' ,
               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
 
             ),
             Container(height: 20,),
 
-
+     
             Text(       
+              
               '[INSERT CONTENT]',
               overflow: TextOverflow.clip,
               style: TextStyle(color: Colors.white),      
@@ -69,7 +96,7 @@ class _TestDataPageState extends State<TestData> {
             ),
 
             Text(
-              'How Can this App Help?',
+              'Your End Time',
               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
 
             ),
